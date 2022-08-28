9513483adc772bc4872995fa46f6e4fa0758029f
Lewis Barnes <lewis_barnes@live.co.uk>
2022-08-28 01:24:28 +0100

    added slate editor

---
+++ src/components/chat/chatEditor.tsx
@@ -0,0 +1,8 @@
+import { FC, createRef,  KeyboardEvent, useEffect, useMemo, useState } from "react";
+import { } from 'react';
+import { BaseEditor, createEditor, Descendant, Node } from 'slate';
+import { Editable, ReactEditor, Slate, withReact } from 'slate-react';
+
+export const ChatEditor: FC = () => {
+	return <div></div>;
+}
